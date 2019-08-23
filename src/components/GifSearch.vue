49d232adf3d6e0bcbe7317c710b81f44a60da09b
Allyson Nason <you@example.com>
2019-08-22 20:10:15 -0600

    initial commit

---
+++ src/components/GifSearch.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <button @click="toggleDropdown">GIF</button>
+        <div v-show="showDropdown">
+            <div>
+                <form @submit.prevent="doSearch">
+                    <input v-model="query" type="text" placeholder="GIF Search" class="form-control" />
+                    <button type="submit" class="btn btn-primary" >Go</button>
+                </form>
+            </div>
+            <div v-if="chosenGIF">
+                <img :src ="chosenGIF.images.fixed_height.url" />
+            </div>
+
+            <div class="image-container">
+                <div class="list-group" >
+                    <result
+                        v-for="(result, index) in results" :image="result.images.fixed_height.url"
+                        @chooseGIFEvent="chooseGIFHandler(index)" />
+
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import axios from 'axios'
+    import Result from './Result.vue'
+
+    export default {
+        components: { Result },
+        data() {
+            return {
+                apiKey: '8OcoGVWE7HzgGUhTkSHYIFCmEHlb1Tp9',
+                showDropdown: false,
+                showResults: false,
+                query: '',
+                results: [],
+                chosenGIF: null
+            }
+        },
+
+        methods: {
+
+            chooseGIFHandler(target) {
+
+                this.chosenGIF = this.results[target];
+                this.showResults = false;
+
+            },
+
+            doSearch() {
+
+                axios.get('https://api.giphy.com/v1/gifs/search?api_key=' + this.apiKey + '&q=' + this.query)
+                    .then( (response) => {
+                        console.log(response.data.data)
+                        this.results = response.data.data;
+
+                })
+            },
+            toggleDropdown() {
+                this.showDropdown = !this.showDropdown;
+
+                if (this.showDropdown == true) {
+                    //this.getTrending();
+                }
+            },
+
+            // getTrending() {
+            //     axios.get('https://api.giphy.com/v1/gifs/search?api_key=' + this.apiKey + '&q=' + this.query)
+            //         .then( (response) => {
+            //             console.log(response.data.data)
+            //             this.results = response.data.data;
+            //
+            //     })
+            // }
+        }
+    }
+
+
+
+</script>
+
+
+
+<style>
+
+.image-container {
+    width: 33.33%;
+    float: left;
+    position: relative;
+}
+
+.list-group {
+  margin-top: 5px;
+  list-style-type: none;
+  overflow: hidden;
+
+  position: absolute;
+  left: 0; top: 50%;
+  width: 100%
+
+}
+
+
+
+</style>
